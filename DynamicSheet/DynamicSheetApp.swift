72c7d6b8b8c8fa7e07e26794da6eb7242866a3c2
vlvsv <vvs_2015@icloud.com>
2025-01-18 11:56:40 +0300

    Initial Commit

---
+++ DynamicSheet/DynamicSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicSheetApp.swift
+//  DynamicSheet
+//
+//  Created by Vlad V on 18.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct DynamicSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
